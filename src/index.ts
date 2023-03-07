0f04aa9a6b68417d44042c42598b9b438ca4d63a
StefaniaSgreva <stefaniasgreva@gmail.com>
2023-03-07 15:57:17 +0100

    ts add new tasks wip

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,51 @@
+import { v4 as uuidV4 } from "uuid"
+
+type Task = {
+  id: string
+  title: string
+  completed: boolean
+  createdAt: Date
+}
+
 const list = document.querySelector<HTMLUListElement>('#list')
 const form = document.getElementById('new_task_form') as HTMLFormElement | null
 const input = document.querySelector<HTMLInputElement>('#new_task_title')
+const tasks: Task[] = []
+
+form?.addEventListener("submit", e =>{
+  e.preventDefault()
+
+  // per verificare se l'input esiste
+  if(input?.value == "" || input?.value == null) return
+
+  const newTask: Task = {
+    id: uuidV4(),
+    title: input.value,
+    completed: false,
+    createdAt: new Date()
+  }
+  tasks.push(newTask)
+
+  addListItem(newTask)
+  input.value = ""
+})
+
+function  addListItem(task: Task){
+  const item = document.createElement("li")
+  const label = document.createElement("label")
+  const checkbox = document.createElement("input")
+  checkbox.addEventListener("change", () => {
+    task.completed = checkbox.checked
+    // console.log(task)
+    saveTasks()
+  })
+  checkbox.type = "checkbox"
+  checkbox.checked = task.completed
+  label.append(checkbox, task.title)
+  item.append(label)
+  list?.append(item)
+}
+
+function  saveTasks(){
+  
+}
